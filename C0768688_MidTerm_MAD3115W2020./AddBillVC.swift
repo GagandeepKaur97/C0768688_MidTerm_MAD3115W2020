1d9cc4aeba9dfc2afb44193baa26c0c3fccb76d9
gagandeep <sherry2312.gk@gmail.com>
2020-03-11 17:05:56 -0400

    [gk] txt fields added for bill type

--- C0768688_MidTerm_MAD3115W2020./AddBillVC.swift
+++ C0768688_MidTerm_MAD3115W2020./AddBillVC.swift
@@ -16,12 +16,26 @@ class AddBillVC: UIViewController {
     
     @IBOutlet weak var billTypeSegLbl: UISegmentedControl!
     @IBOutlet weak var datePickerLbl: UIDatePicker!
+    
+    @IBOutlet weak var txtFeild1: UITextField!
+    @IBOutlet weak var txtFeild2: UITextField!
+    @IBOutlet weak var txtFeild3: UITextField!
+    @IBOutlet weak var txtFeild4: UITextField!
+    @IBOutlet weak var txtFeild5: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         datePickerLbl.isHidden = true
         billTypeSegLbl.isHidden = true
+        txtFeild1.isHidden = true
+        txtFeild2.isHidden = true
+        txtFeild3.isHidden = true
+        txtFeild4.isHidden = true
+        txtFeild5.isHidden = true
         
     }
     
@@ -51,11 +65,55 @@ class AddBillVC: UIViewController {
     }
     
     
-    
     @IBAction func billTypeAction(_ sender: Any) {
+        
+        switch billTypeSegLbl.selectedSegmentIndex {
+        case 0:
+            billTypeTxtFld.text = "Hydro"
+            txtFeild1.placeholder = "agency name"
+            txtFeild2.placeholder = "units consumed"
+            txtFeild1.isHidden = false
+            txtFeild2.isHidden = false
+            
+            txtFeild3.isHidden = true
+            txtFeild4.isHidden = true
+            txtFeild5.isHidden = true
+        case 1:
+            billTypeTxtFld.text = "Mobile"
+            txtFeild1.placeholder = "provider name"
+            txtFeild2.placeholder = "GB used"
+            txtFeild1.isHidden = false
+            txtFeild2.isHidden = false
+            
+            txtFeild3.isHidden = true
+            txtFeild4.isHidden = true
+            txtFeild5.isHidden = true
+            
+        case 2:
+            billTypeTxtFld.text = "Internet"
+            
+            txtFeild1.placeholder = "manufacturer name"
+            txtFeild2.placeholder = "plan name"
+            txtFeild3.placeholder = "mobile number"
+            txtFeild4.placeholder = "GB used"
+            txtFeild5.placeholder = "minutes used"
+            
+            txtFeild1.isHidden = false
+            txtFeild2.isHidden = false
+            txtFeild3.isHidden = false
+            txtFeild4.isHidden = false
+            txtFeild5.isHidden = false
+            
+            
+            
+        default:
+            break
+        }
     }
     
 
+    
+    
     @IBAction func dateTxtFldAction(_ sender: Any) {
           datePickerLbl.isHidden = false
     }
