db4cfb1430eacb3a69f862add9c016d9ee715038
gagan <sherry2312.gk@gmail.com>
2020-03-13 13:59:17 -0400

    [gk] added table cell

---
+++ C0768688_MidTerm_MAD3115W2020./CustomerTableViewcellDVc.swift
@@ -0,0 +1,24 @@
+//
+//  CustomerTableViewcellDVc.swift
+//  C0768688_MidTerm_MAD3115W2020.
+//
+//  Created by MacStudent on 2020-03-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class CustomerTableViewcellDVc: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
